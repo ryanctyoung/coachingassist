1f1008a15510532bb6f4032c932605ba7ae303f6
Ryan Young <lightning16ry@gmail.com>
2020-03-27 12:22:25 -0700

    initalization of project

---
+++ client/pages/components/landing/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Landing() {
+  return (
+    <div>
+      Coach's Challenge
+    </div>
+  );
+}
---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+import Landing from "./components/landing"
+
+
+export default function Index() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
